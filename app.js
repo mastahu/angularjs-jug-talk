e41f60e7c1c5969cdd914e8fd659b4ebc9ec4e6b
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-06-26 22:16:52 +0200

    Model moved to service

--- app.js
+++ app.js
@@ -2,16 +2,27 @@
 
 var app = angular.module('app', []);
 
-app.controller('PeopleCtrl', function($scope) {
+app.controller('PeopleCtrl', function($scope, registeredAttendees) {
 
-	$scope.people = [
-		{name: 'John Doe', email: 'john@doe.com', confirmed: true},
-		{name: 'Mary Smith', email: 'mary@smith.com', confirmed: false}
-	];
+	$scope.people = registeredAttendees.attendees;
 
 	$scope.addPerson = function() {
-		$scope.people.push($scope.newAttendee);
+		registeredAttendees.addAttendee($scope.newAttendee);
 		delete $scope.newAttendee;
 	}
+
+});
+
+app.service('registeredAttendees', function() {
+
+	this.attendees = [
+		{name: 'John Doe', email: 'john@doe.com', confirmed: true},
+		{name: 'Mary Smith', email: 'mary@smith.com', confirmed: false},
+		{name: 'Bob Brown', email: 'bob@brown.com', confirmed: true}
+	];
+
+	this.addAttendee = function(attendee) {
+		this.attendees.push(attendee);
+	};
 
 });

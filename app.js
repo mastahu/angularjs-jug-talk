04c15205db096b1f258efa95a5b0cd233f12e4bc
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-06-26 22:05:58 +0200

    Model to object instead of simple string

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = angular.module('app', []);
 
 app.controller('PeopleCtrl', function($scope) {
 
-	$scope.people = ['John Doe', 'Mary Smith'];
+	$scope.people = [
+		{name: 'John Doe', email: 'john@doe.com', confirmed: true},
+		{name: 'Mary Smith', email: 'mary@smith.com', confirmed: false}
+	];
 
 	$scope.addPerson = function() {
 		$scope.people.push($scope.newAttendee);
+		delete $scope.newAttendee;
 	}
 
 });
